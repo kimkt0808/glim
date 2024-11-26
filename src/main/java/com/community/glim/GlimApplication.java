9f2c7ab8aa7ffbf6f6fb466de7419007f73d2358
kimkt0808 <kookb20000@naver.com>
2024-11-26 13:05:18 +0900

    first commit

---
+++ src/main/java/com/community/glim/GlimApplication.java
@@ -0,0 +1,13 @@
+package com.community.glim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/community/glim/GlimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.glim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
